dff00adadd69422ffe061d501849278d659a4124
flyether <flyether@gmail.com>
2022-11-09 14:22:22 +0300

    feat: [#4] moving header to app

--- src/components/GlobalRoute.tsx
+++ src/components/GlobalRoute.tsx
@@ -4,14 +4,12 @@ import { HashRouter, Route, Routes } from 'react-router-dom';
 import ErrorPage from '../pages/ErrorPage/ErrorPage';
 import LoginPage from '../pages/LoginPage/LoginPage';
 import RegisterPage from '../pages/RegisterPage/RegisterPage';
-import Header from './Header/Header';
 
 const GlobalRoute = () => {
   return (
     <>
       {' '}
       <HashRouter>
-        <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="*" element={<ErrorPage />} />
