7e084f41efcb8d877ce8692f1d6353a8887db2c8
Aldar <aldarkonov@mail.ru>
2022-11-17 11:10:29 +0300

    feat: [#29] implement logout button to Header, change button font size

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,23 +3,58 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 
 import styles from './Navigation.module.scss';
+import { removeUser, useAppDispatch, useAppSelector } from 'store';
+import Button from 'components/atoms/Button';
 
 const Navigation = () => {
   const { t } = useTranslation();
 
+  const { token } = useAppSelector((state) => state.user);
+  const dispatch = useAppDispatch();
+
+  const handleSignout = async () => {
+    if (token) {
+      dispatch(removeUser());
+      localStorage.removeItem('token');
+    }
+  };
+
   return (
     <nav className={styles.container}>
       <ul className={styles.list}>
-        <li className={styles.item}>
-          <Link to="/login" className={styles.link}>
-            {t('Navigation.signIn')}
-          </Link>
-        </li>
-        <li className={styles.item}>
-          <Link to="/registration" className={styles.link}>
-            {t('Navigation.signUp')}
-          </Link>
-        </li>
+        {token ? (
+          <li className={styles.item}>
+            <Button
+              text={t('Navigation.signOut')}
+              type="secondary"
+              big={false}
+              onClick={handleSignout}
+            />
+          </li>
+        ) : (
+          <>
+            <li className={styles.item}>
+              <Link to="/login" className={styles.link}>
+                <Button
+                  text={t('Navigation.signIn')}
+                  type="secondary"
+                  big={false}
+                  onClick={handleSignout}
+                />
+              </Link>
+            </li>
+            <li className={styles.item}>
+              <Link to="/registration" className={styles.link}>
+                <Button
+                  text={t('Navigation.signUp')}
+                  type="secondary"
+                  big={false}
+                  onClick={handleSignout}
+                />
+              </Link>
+            </li>
+          </>
+        )}
       </ul>
     </nav>
   );
