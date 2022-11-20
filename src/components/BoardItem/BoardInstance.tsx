862f4ee6e271f35afb4f9eeb73bd79dae558feb4
lebedev-023046 <lebedev.023046@gmail.com>
2022-11-21 00:45:20 +0300

    feat: [#19] add Board view

---
+++ src/components/BoardItem/BoardInstance.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import styles from './BoardItem.module.scss';
+import { Link } from 'react-router-dom';
+
+interface IBoardInstanceProps {
+  board: {
+    _id: string;
+    title: string;
+    description: string;
+  };
+}
+
+const BoardInstance = ({ board }: IBoardInstanceProps) => {
+  return (
+    <Link to={`/boards/${board._id}`} className={`${styles.board} ${styles.instance}`}>
+      <div className={styles.img}></div>
+      <div className={styles.info}>
+        <div className={styles.topInfo}>
+          <h3 className={styles.title}>{board.title}</h3>
+          <div className={styles.controls}>
+            <button
+              className={`${styles.buttonControl} ${styles.edit}`}
+              onClick={(e) => {
+                e.preventDefault();
+                console.log('edit');
+              }}
+            ></button>
+            <button
+              className={`${styles.buttonControl} ${styles.delete}`}
+              onClick={(e) => {
+                e.preventDefault();
+                console.log('delete');
+              }}
+            ></button>
+          </div>
+        </div>
+        <div className={styles.descr}>{board.description}</div>
+      </div>
+    </Link>
+  );
+};
+
+export default BoardInstance;
