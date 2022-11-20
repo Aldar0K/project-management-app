866929cf978a67d395065065bf26a765cebe033c
lebedev-023046 <lebedev.023046@gmail.com>
2022-11-21 00:41:05 +0300

    feat: [#19] add main page route

--- src/components/GlobalRoute/GlobalRoute.tsx
+++ src/components/GlobalRoute/GlobalRoute.tsx
@@ -4,16 +4,17 @@ import WelcomePage from 'pages/WelcomePage';
 import ErrorPage from 'pages/ErrorPage';
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
+import MainPage from 'pages/MainPage';
 
 const GlobalRoute = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-        {/* <Route path="/main" element={<MainPage />} /> */}
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registration" element={<RegisterPage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
   );
