bc0b1511b41881acd047c5232a68288ddfc2886d
lebedev-023046 <lebedev.023046@gmail.com>
2022-11-21 00:40:43 +0300

    feat: [#19] add main page view details

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,11 +1,44 @@
+import BoardInstance from 'components/BoardItem/BoardInstance';
+import BoardPlus from 'components/BoardItem/BoardPlus';
+import CreateBoardModal from 'components/BoardItem/CreateBoardModal/CreateBoardModal';
 import React from 'react';
-import { Link } from 'react-router-dom';
+import styles from './MainPage.module.scss';
+
+//mock data
+const fakeApi = [
+  {
+    _id: '1',
+    title: 'first',
+    description: 'board 1',
+  },
+  {
+    _id: '2',
+    title: 'second',
+    description: 'board 2',
+  },
+  {
+    _id: '3',
+    title: 'third',
+    description: 'board 3',
+  },
+];
 
 const MainPage = () => {
   return (
-    <div>
-      <h1>Boards page</h1>
-      <Link to="/login"> login </Link>
+    <div className="main">
+      <div className={`container`}>
+        <ul className={styles.container}>
+          {fakeApi.map((board) => (
+            <li key={board._id}>
+              <BoardInstance board={board} />
+            </li>
+          ))}
+          <li>
+            <BoardPlus />
+          </li>
+        </ul>
+        {/* <CreateBoardModal /> */}
+      </div>
     </div>
   );
 };
