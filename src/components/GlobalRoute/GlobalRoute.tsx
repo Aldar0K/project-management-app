2642f369f754f5d0598b933a4ab05a584314425d
lebedev-023046 <lebedev.023046@gmail.com>
2022-11-21 01:11:47 +0300

    feat: [#19] change routes order

--- src/components/GlobalRoute/GlobalRoute.tsx
+++ src/components/GlobalRoute/GlobalRoute.tsx
@@ -4,26 +4,18 @@ import WelcomePage from 'pages/WelcomePage';
 import ErrorPage from 'pages/ErrorPage';
 import LoginPage from 'pages/LoginPage';
 import RegisterPage from 'pages/RegisterPage';
-<<<<<<< HEAD
 import MainPage from 'pages/MainPage';
-=======
 import EditProfilePage from 'pages/EditProfilePage';
->>>>>>> develop
 
 const GlobalRoute = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<WelcomePage />} />
-<<<<<<< HEAD
-        <Route path="/main" element={<MainPage />} />
-=======
-        {/* <Route path="/main" element={<MainPage />} /> */}
-        <Route path="/editProfile" element={<EditProfilePage />} />
-        <Route path="*" element={<ErrorPage />} />
->>>>>>> develop
         <Route path="/login" element={<LoginPage />} />
         <Route path="/registration" element={<RegisterPage />} />
+        <Route path="/editProfile" element={<EditProfilePage />} />
+        <Route path="/main" element={<MainPage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
     </>
