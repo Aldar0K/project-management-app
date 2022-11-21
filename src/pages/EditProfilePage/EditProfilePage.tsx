b6ad0fec53af86bf46db4be8977a523b1041189c
flyether <flyether@gmail.com>
2022-11-21 10:36:05 +0300

    feat: [#34] add styles container

--- src/pages/EditProfilePage/EditProfilePage.tsx
+++ src/pages/EditProfilePage/EditProfilePage.tsx
@@ -1,10 +1,11 @@
 import EditProfile from 'components/EditProfile/EditProfile';
 import React from 'react';
+import styles from './styleContainer.module.scss';
 
 const EditProfilePage = () => {
   return (
     <main className="main">
-      <div className="container">
+      <div className={`container ${styles.container}`}>
         <EditProfile />
       </div>
     </main>
