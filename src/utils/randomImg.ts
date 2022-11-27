bdce5d9297a7a89e1ab9f0f6347ca71aba549af8
lebedev-023046 <lebedev.023046@gmail.com>
2022-11-27 23:21:09 +0300

    refactor: [#19] change random img to one img

--- src/utils/randomImg.ts
+++ src/utils/randomImg.ts
@@ -1,3 +1,4 @@
 export default function getImgPath() {
-  return require(`../assets/images/jpg/boardImgs/city${Math.floor(Math.random() * 5 + 1)}.jpg`);
+  // return require(`../assets/images/jpg/boardImgs/city${Math.floor(Math.random() * 5 + 1)}.jpg`);
+  return require(`../assets/images/jpg/boardImgs/city5.jpg`);
 }
